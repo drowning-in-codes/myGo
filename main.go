7c2e7b1638dab78eb24df68ab072a57ad194dea4
proanimer <1322767102@qq.com>
2024-01-16 10:12:35 +0800

    initial

--- main.go
+++ main.go
@@ -12,3 +12,17 @@ func main() {
 
 	router.Run(":8080")
 }
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"sekyoro.top/Goimg/routes"
+)
+
+func main() {
+	router := gin.Default()
+	routes.DownloadPicRoutes(router)
+	routes.ShowPicRoutes(router)
+
+	router.Run(":8080")
+}
